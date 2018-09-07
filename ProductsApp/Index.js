50f8a41e7d2523982ad0f13fc202df3dc0657845
dbarraza <32948749+dbarraza@users.noreply.github.com>
2018-09-07 17:05:26 -0300

    Add files via upload

---
+++ ProductsApp/Index.js
@@ -0,0 +1,38 @@
+﻿var uri = 'api/products';
+
+$(function () {
+    // Send an AJAX request
+    $.getJSON(uri)
+        .done(function (data) {
+            // On success, 'data' contains a list of products.
+            $.each(data, function (key, item) {
+                // Add a list item for the product.
+                $('<li>', { text: formatItem(item) }).appendTo($('#products'));
+            });
+        });
+
+    $("#btnSearch").click(function() {
+        find();
+    });
+});
+
+function formatItem(item) {
+    return item.Name + ': $' + item.Price;
+}
+
+function find() {
+
+    var id = $('#prodId').val();
+
+    if (isNaN(id) || id === '') {
+        return false;
+    }
+
+    $.getJSON(uri + '/' + id)
+        .done(function (data) {
+            $('#product').text(formatItem(data));
+        })
+        .fail(function (jqXHR, textStatus, err) {
+            $('#product').text('Error: ' + err);
+        });
+}
